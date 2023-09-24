b06ca2d51ac187d0a9a0c1ef6cfc38ba467b3e07
rajib4375 <rajibhossain10498@gmail.com>
2023-09-25 02:14:44 +0600

    added navbar section

---
+++ src/Pages/Statistics/Statistics.jsx
@@ -0,0 +1,11 @@
+
+
+const Statistics = () => {
+    return (
+        <div>
+            Statistics Statistics
+        </div>
+    );
+};
+
+export default Statistics;
---
+++ src/components/Header/Navbar/Logo.jsx
@@ -0,0 +1,11 @@
+
+
+const Logo = () => {
+    return (
+        <div>
+            <img src="https://i.ibb.co/qBMkH8j/Logo.png" alt="" />
+        </div>
+    );
+};
+
+export default Logo;
